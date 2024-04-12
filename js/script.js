573430e782f311e49bf0c9766c9d298549277c00
R.Jones <RayshawnJones1991@gmail.com>
2024-04-12 13:16:30 -0400

    2nd build of hit-the-button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-/*-------------- Constants -------------*/
-
-const button = document.querySelector('button'); // Select the first button element
-
-const paragraph = document
+// Selects first button
+const button = document.querySelector('button');
 
 const audio = document.createElement('audio');
 
@@ -10,30 +7,8 @@ const image = document.createElement('img');
 
 const h1 = document.querySelector('h1');
 
-
-/*---------- Variables (state) ---------*/
-
-
-/*----- Cached Element References  -----*/
-
-
-/*-------------- Functions -------------*/
-
-//summary,:when the DOM is fully loaded, this script will find the first button on the page and set up a click event. When the button is clicked, a new paragraph saying 'You win a cookie!' will be created and appended to the end of the document body.
-
-
-
-//Line 7 has an event listener that waits for the 'DOMContentLoaded' event. Activates when the initial HTML document has been fully loaded and parsed. It doesn't wait for stylesheets, images, and subframes to finish loading.
-
-document.addEventListener('DOMContentLoaded', (event) => { // Wait for the DOM to load 
-
-    // Get the button element from the DOM
-    // const button = document.querySelector('button'); // Select the first button element
-
-    // The function provided as the second argument to addEventListener will be executed whenever the button is clicked.
-    const handleClick = () => { // When the button is clicked
-
-        // Remove the event listener from the button
+document.addEventListener('DOMContentLoaded', (event) => {
+    const handleClick = () => { 
         button.removeEventListener('click', handleClick);
         h1.remove();
         button.remove();
@@ -42,7 +17,7 @@ document.addEventListener('DOMContentLoaded', (event) => { // Wait for the DOM t
         const audio = document.createElement('audio');
 
         // Set the source of the audio file
-        audio.src = './BTN/jumpscare-154489.mp3';
+        audio.src = './assets/jumpscare-154489.mp3';
 
         // Play the audio when the button is clicked
         audio.play();
@@ -56,13 +31,8 @@ document.addEventListener('DOMContentLoaded', (event) => { // Wait for the DOM t
         // Append the paragraph to the body element
         document.body.appendChild(paragraph);
 
-        // Create an image element
-        const image = document.createElement('img');
+        image.src = './assets/images/cookie-monster.jpg';
 
-        // Set the source of the image file
-        image.src = '/Images/Cookie Monster .webp';
-
-        // Append the image to the paragraph element
         paragraph.appendChild(image);
     };
 
